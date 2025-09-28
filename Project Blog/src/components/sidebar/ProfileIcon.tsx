039fd5663347045b2ac52adf11e105b753fe3524
Benson Ang <bensonang36@gmail.com>
2025-09-29 03:20:06 +0800

    update iamge links

--- Project Blog/src/components/sidebar/ProfileIcon.tsx
+++ Project Blog/src/components/sidebar/ProfileIcon.tsx
@@ -1,7 +1,7 @@
 export const ProfileIcon = () => {
     return (
         <div className="flex flex-col items-center justify-center text-center">
-            <img src="/profile-pic.jpg" alt="Profile" className="w-24 h-24 rounded-full" />
+            <img src={`${import.meta.env.BASE_URL}/profile-pic.jpg`} alt="Profile" className="w-24 h-24 rounded-full" />
             <span className="mt-2 text-gray-800 dark:text-white">Benson</span>
         </div>
     );
