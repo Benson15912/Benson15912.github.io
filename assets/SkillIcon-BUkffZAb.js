a81ec27b463c5a5cba48d60cdc38bc7249d0bc39
Benson Ang <bensonang36@gmail.com>
2025-09-29 03:11:42 +0800

    Updates

---
+++ assets/SkillIcon-BUkffZAb.js
@@ -0,0 +1,11 @@
+import{j as i}from"./index-CrL1MAxJ.js";const c={JavaScript:"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",TypeScript:"https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",React:"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg","Node.js":"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",Python:"https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",Java:"https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",Spring:"https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg",SQL:"https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",RabbitMQ:"https://raw.githubusercontent.com/devicons/devicon/master/icons/rabbitmq/rabbitmq-original.svg",PyTorch:"https://raw.githubusercontent.com/devicons/devicon/master/icons/pytorch/pytorch-original.svg",HTML:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",CSS:"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",HuggingFace:"https://huggingface.co/datasets/huggingface/brand-assets/resolve/main/hf-logo.svg",Selenium:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Selenium_Logo.png/1148px-Selenium_Logo.png"},a=({skill:s,size:t="md",className:n=""})=>{const o=c[s];if(!o)return i.jsx("div",{className:`
+        inline-flex items-center justify-center rounded-lg bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-400 font-medium
+        ${t==="sm"?"w-6 h-6 text-xs":""}
+        ${t==="md"?"w-8 h-8 text-sm":""}
+        ${t==="lg"?"w-12 h-12 text-base":""}
+        ${n}
+      `,children:s.charAt(0).toUpperCase()});const e={sm:"w-6 h-6",md:"w-8 h-8",lg:"w-12 h-12"};return i.jsx("img",{src:o,alt:`${s} icon`,className:`
+        rounded-lg object-contain
+        ${e[t]}
+        ${n}
+      `,loading:"lazy"})};export{a as S};
